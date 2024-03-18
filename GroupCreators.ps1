bc481db94de06de6a8d83a7390af7dcb056c1c68
dntt3:lQ <44235375+danttelucorem@users.noreply.github.com>
2024-03-18 15:49:31 +0100

    Create GroupCreators.ps1

---
+++ GroupCreators.ps1
@@ -0,0 +1,42 @@
+Import-Module Microsoft.Graph.Beta.Identity.DirectoryManagement
+Import-Module Microsoft.Graph.Beta.Groups
+
+Connect-MgGraph -Scopes "Directory.ReadWrite.All", "Group.Read.All"
+
+$GroupName = "GroupCreators-MS365"
+$AllowGroupCreation = "False"
+
+$settingsObjectID = (Get-MgBetaDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).id
+
+if(!$settingsObjectID)
+{
+    $params = @{
+	  templateId = "62375ab9-6b52-47ed-826b-58e47e0e304b"
+	     }
+	
+    New-MgBetaDirectorySetting -BodyParameter $params
+	
+    $settingsObjectID = (Get-MgBetaDirectorySetting | Where-object -Property Displayname -Value "Group.Unified" -EQ).Id
+}
+
+$groups = Get-MgBetaGroup -All
+ 
+$groupId = ($groups | Where-object {$_.displayname -eq $GroupName}).Id
+
+$params = @{
+	templateId = "62375ab9-6b52-47ed-826b-58e47e0e304b"
+	values = @(
+		@{
+			name = "EnableGroupCreation"
+			value = $AllowGroupCreation
+		}
+		@{
+			name = "GroupCreationAllowedGroupId"
+			value = $groupId
+		}
+	)
+}
+
+Update-MgBetaDirectorySetting -DirectorySettingId $settingsObjectID -BodyParameter $params
+
+(Get-MgBetaDirectorySetting -DirectorySettingId $settingsObjectID).Values
